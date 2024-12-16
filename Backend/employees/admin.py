6c19d161a2c39ace66deb831f9f6895c29dcc1c6
Muhammad Aneeq <165320478+aneeq769@users.noreply.github.com>
2024-12-16 08:35:20 +0500

    Backend directory

--- Backend/employees/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import Task
-
-@admin.register(Task)
-class TaskAdmin(admin.ModelAdmin):
-    list_display = ('title', 'assigned_to', 'status', 'priority', 'due_date', 'created_at')
-    list_filter = ('status', 'priority', 'assigned_to')
-    search_fields = ('title', 'description', 'assigned_to__username')
-    ordering = ('-created_at',)
